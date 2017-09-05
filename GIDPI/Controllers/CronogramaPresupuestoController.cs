0cbfd0fe448bed5d513d639d45ef400b993ba505
ADraWone <avelez90@misena.edu.co>
2017-09-05 07:34:35 -0500

    lo que se me olvido :v:

--- GIDPI/Controllers/CronogramaPresupuestoController.cs
+++ GIDPI/Controllers/CronogramaPresupuestoController.cs
@@ -12,5 +12,25 @@ namespace GIDPI.Controllers
 {
     public class CronogramaPresupuestoController:ApiController
     {
+        [HttpPost]
+        public IHttpActionResult GuardarCronograma(List<Involucrados> oListaInvolucrados)
+        {
+            try
+            {
+                InvolucradosBL oInvolucradosBL = new InvolucradosBL();
+                oInvolucradosBL.GuardarInvolucrados(oListaInvolucrados);
+
+                return Ok(new { success = true });
+
+            }
+            catch (Exception e)
+            {
+
+                return Ok(new { success = false, e.Message });
+
+            }
+        }
+
+
     }
 }
