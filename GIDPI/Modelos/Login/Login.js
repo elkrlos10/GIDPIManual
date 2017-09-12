48831d13fd4911285600fc7ccd9dc8b8f451fc47
ADraWone <avelez90@misena.edu.co>
2017-09-12 15:59:56 -0500

    se me olvido montarlo :pig:

--- GIDPI/Modelos/Login/Login.js
+++ GIDPI/Modelos/Login/Login.js
@@ -36,25 +36,25 @@
             $scope.RegistroPersonaNatural = function () {
 
                 if ($scope.PersonaNatural.Nombre == null || $scope.PersonaNatural.Nombre == ""  ) {
-                    alertify.error("El campo Nombre no puede estar vacio");
+                    alertify.success("El campo Nombre no puede estar vacio");
                     return false;
                 } else if ($scope.PersonaNatural.Apellidos == null || $scope.PersonaNatural.Apellidos == "") {
-                    alertify.error("El campo Apellidos no puede estar vacio");
+                    alertify.success("El campo Apellidos no puede estar vacio");
                     return false;
-
-
+                
+                
                 }else  if ($scope.PersonaNatural.Documento == null || $scope.PersonaNatural.Documento == ""  ) {
-                    alertify.error("El campo Documento no puede estar vacio");
+                    alertify.success("El campo Documento no puede estar vacio");
                     return false;
                 }else  if ($scope.PersonaNatural.Usuario == null || $scope.PersonaNatural.Usuario == ""  ) {
-                    alertify.error("El campo Usuario no puede estar vacio");
+                    alertify.success("El campo Usuario no puede estar vacio");
                     return false;
                 
                 } else if ($scope.PersonaNatural.Contrasena == null || $scope.PersonaNatural.Contrasena == "") {
-                    alertify.error("El campo Contraseña no puede estar vacio");
+                    alertify.success("El campo Contraseña no puede estar vacio");
                     return false;
                 }else if ($scope.PersonaNatural.Email == null || $scope.PersonaNatural.Email == ""  ) {
-                    alertify.error("El campo Email no puede estar vacio o debe tener una direccion correcta");
+                    alertify.success("El campo Email no puede estar vacio o debe tener una direccion correcta");
                     return false;
           
                 }else {
@@ -73,30 +73,30 @@
             $scope.RegistroPersonaJuridica = function () {
 
                 if ($scope.PersonaJuridica.RazonSocial == null || $scope.PersonaJuridica.RazonSocial == "") {
-                    alertify.error("El campo RazonSocial no puede estar vacio");
+                    alertify.success("El campo RazonSocial no puede estar vacio");
                     return false;
                 } else if ($scope.PersonaJuridica.Nit == null || $scope.PersonaJuridica.Nit == "") {
-                    alertify.error("El campo Nit no puede estar vacio");
+                    alertify.success("El campo Nit no puede estar vacio");
                     return false;
 
 
                 }else  if ($scope.PersonaJuridica.SectorEconomico == null || $scope.PersonaJuridica.SectorEconomico == ""  ) {
-                    alertify.error("El campo SectorEconomico no puede estar vacio");
+                    alertify.success("El campo SectorEconomico no puede estar vacio");
                     return false;
                 }else  if ($scope.PersonaJuridica.Responsable == null || $scope.PersonaJuridica.Responsable == ""  ) {
-                    alertify.error("El campo Responsable no puede estar vacio");
+                    alertify.success("El campo Responsable no puede estar vacio");
                     return false;
                 
              
                 } else if ($scope.PersonaJuridica.Email == null || $scope.PersonaJuridica.Email == "") {
-                    alertify.error("El campo Email no puede estar vacio o debe tener una direccion correcta");
+                    alertify.success("El campo Email no puede estar vacio o debe tener una direccion correcta");
                     return false;
 
                 }else  if ($scope.PersonaJuridica.Usuario == null || $scope.PersonaJuridica.Usuario == ""  ) {
-                    alertify.error("El campo Usuario no puede estar vacio");
+                    alertify.success("El campo Usuario no puede estar vacio");
                     return false;
                 }else  if ($scope.PersonaJuridica.Contrasena == null || $scope.PersonaJuridica.Contrasena == ""  ) {
-                    alertify.error("El campo contraseña no puede estar vacio");
+                    alertify.success("El campo contraseña no puede estar vacio");
                     return false;
                 } else {
                     LoginService.RegistroPersonaJuridica($scope.PersonaJuridica, function (response) {
@@ -116,11 +116,11 @@
 
             $scope.ConsultarUsuario = function () {
                 if ($scope.Usuario.Usuario1 == null || $scope.Usuario.Usuario1 == "") {
-                    alertify.error("Necesita ingresar el campo de Usuario");
+                    alertify.success("Necesita ingresar el campo de Usuario");
                     return false;
 
                 } else if ($scope.Usuario.Contrasena == null || $scope.Usuario.Contrasena == "") {
-                    alertify.error("Necesita ingresar el campo de contraseña");
+                    alertify.success("Necesita ingresar el campo de contraseña");
                     return false;
                 } else {
                     LoginService.ConsultarUsuario($scope.Usuario, function (response) {
