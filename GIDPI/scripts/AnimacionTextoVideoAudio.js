874c490e6cf1bd2a9d56fa05b5cea726ec0918bc
MariaCamilaUZ <mariacamila0871@gmail.com>
2017-09-25 08:35:35 -0500

    Menú Ayuda
    Ayudas de texto en vistas

---
+++ GIDPI/scripts/AnimacionTextoVideoAudio.js
@@ -0,0 +1,99 @@
+﻿//-------------INICIA MENÚ PRINCIPAL---------------------
+
+$('#menu-toggle').click(function () {
+     $('#wrapper').slideToggle({
+         direction: "up"
+     }, 300);
+     $(this).toggleClass('clientsClose');
+ });
+
+
+$(document).ready(function(){
+    $("#audio").click(function () {
+        $("#audioWrap").each(function () {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+
+$(function () {
+
+    $('.demo5').svgpopup({
+        figure: 'rectangle',
+        fill: '#238276',
+        randomize: false
+    });
+
+    $('.demo6').svgpopup({
+        figure: 'rectangle',
+        fill: '#238276',
+        randomize: false
+    });
+
+}());
+
+
+
+$(function() {
+    var player = $.AudioPlayer;
+    player.init({
+        container: '#audioWrap'
+        ,source: '../audios/Don Tetto - Yo Estaré Bien (Con Letra).mp3'
+        ,imagePath: '../images'
+        ,debuggers: false
+        ,allowSeek: true
+    });
+
+    $('[data-url]').on('click', function(event) {
+        event.preventDefault();
+
+        player.updateSource({
+            source: $(this).data('url')
+        });
+    });
+});
+
+//-------------FINALIZA MENÚ PRINCIPAL---------------------
+
+
+//------------- INICIA DATOS PROYECTO----------------------
+
+$(function () {
+
+    $('.ayudaTextoUno').svgpopup({
+        figure: 'rectangle',
+        fill: '#238276',
+        randomize: false
+    });
+
+
+    $('.ayudaVideoUno').svgpopup({
+        figure: 'rectangle',
+        fill: '#238276',
+        randomize: false
+    });
+
+}());
+
+//------------- FINALIZA DATOS PROYECTO----------------------
+
+
+
+//------------- INICIA MATRIZ DE VESTER----------------------
+
+
+
+
+
+
+//------------- FINALIZA MATRIZ DE VESTER----------------------
