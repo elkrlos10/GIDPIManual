1c1b30234b1635fda6f42b0e9cff8cf40f7a2674
MariaCamilaUZ <mariacamila0871@gmail.com>
2017-09-27 11:46:33 -0500

    Modificaciones
    Modificaciones generales

--- GIDPI/scripts/AnimacionTips.js
+++ GIDPI/scripts/AnimacionTips.js
@@ -1,12 +1,63 @@
-﻿//------------------------TIPS MATRIZ DE INVOLUCRADOS--------------------------------------------
+﻿//------------------------INICIA TIPS ÁRBOL DE OBJETIVOS--------------------------------------------
 
+//-----------------------INICIA TIPS OBJETIVOS TAB 1-------------------------------------------------------
 
-function usage() {
-    $.notify("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet arcu est, eu tincidunt tellus scelerisque in. Donec elementum commodo nunc. Nulla gravida pretium ante, nec vulputate turpis luctus ut. Sed ligula arcu, tincidunt eu imperdiet non, iaculis ac mauris. Praesent porttitor dapibus magna faucibus aliquet. Nunc commodo elementum ante non mollis. Sed viverra elit quis placerat ultricies. Cras et urna tristique, ornare nulla vel, placerat est. ",
+function tipObjetivos() {
+    $.notify("<h5 style='text-align:center'><b>Para recordar...</b></h5>" + "Verbo en infinitivo + sujeto + condición + criterio",
 
         {
-            type: "success", align: "center", verticalAlign: "bottom",
+            type: "success", align: "left", verticalAlign: "bottom",
+            delay: 10000, animation: true, animationType: "scale", close: true
+
+        });+
+
+
+function tipObjetivosDos() {
+    $.notify("<h5 style='text-align:center'><b>Para recordar...</b></h5>" + "Ningún objetivo debe contener dos o más verbos en infinitivo.",
+
+        {
+            type: "success", align: "left", verticalAlign: "bottom",
             delay: 10000, animation: true, animationType: "scale", close: true
 
         });
 }
+
+
+function tipObjetivosTres() {
+    $.notify("<h5 style='text-align:center'><b>Para recordar...</b></h5>" + "El objetivo general y los objetivos específicos deben tener los siguientes requisitos  según la Metodología SMART ideada por George T. Doran.",
+
+        {
+            type: "success", align: "left", verticalAlign: "bottom",
+            delay: 10000, animation: true, animationType: "scale", close: true
+
+        });
+}
+
+
+//-----------------------FINALIZA TIPS OBJETIVOS TAB 1-------------------------------------------------------
+
+
+
+
+//------------------------FINALIZA TIPS ÁRBOL DE OBJETIVOS--------------------------------------------
+
+
+
+
+
+
+
+
+//------------------------INICIA TIPS MATRIZ DE INVOLUCRADOS--------------------------------------------
+
+
+function usage() {
+    $.notify("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet arcu est,eu tincidunt tellus scelerisque in. Donec elementum commodo nunc. Nulla gravida pretium ante, nec vulputate turpis luctus ut. Sed ligula arcu, tincidunt eu imperdiet non, iaculis ac mauris. Praesent porttitor dapibus magna faucibus aliquet. Nunc commodo elementum ante non mollis. Sed viverra elit quis placerat ultricies. Cras et urna tristique, ornare nulla vel, placerat est. ",
+
+        {
+            type: "success", align: "left", verticalAlign: "bottom",
+            delay: 10000, animation: true, animationType: "scale", close: true
+
+        });
+}
+//------------------------FINALIZA TIPS MATRIZ DE INVOLUCRADOS--------------------------------------------
