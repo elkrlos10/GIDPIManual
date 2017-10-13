aa05efb78e1ff87e381daeba777bffeef7c701a0
ADraWone <avelez90@misena.edu.co>
2017-10-13 10:34:35 -0500

    cambios del reporte
    falta acomodar bien las apginas

--- GIDPI/ReporteRcdl/VistaInforme.aspx.cs
+++ GIDPI/ReporteRcdl/VistaInforme.aspx.cs
@@ -13,19 +13,19 @@ namespace GIDPI.ReporteRcdl
         {
             var IdProyecto = Request.QueryString["IdProyecto"];
 
-            SqlDataSource1.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource2.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource3.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource4.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource5.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource6.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource7.SelectParameters["IdProyecto"].DefaultValue = "97";
-            SqlDataSource8.SelectParameters["IdProyecto"].DefaultValue = "97";
-            SqlDataSource9.SelectParameters["IdProyecto"].DefaultValue = "65";
-            SqlDataSource10.SelectParameters["IdProyecto"].DefaultValue = "65";
-            SqlDataSource11.SelectParameters["IdProyecto"].DefaultValue = "66";
-            SqlDataSource12.SelectParameters["IdProyecto"].DefaultValue = "49";
-            SqlDataSource13.SelectParameters["IdProyecto"].DefaultValue = "66";
+            SqlDataSource1.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource2.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource3.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource4.SelectParameters["IdProyecto"].DefaultValue =IdProyecto;
+            SqlDataSource5.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource6.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource7.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource8.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource9.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
+            SqlDataSource10.SelectParameters["IdProyecto"].DefaultValue =IdProyecto;
+            SqlDataSource11.SelectParameters["IdProyecto"].DefaultValue =IdProyecto;
+            SqlDataSource12.SelectParameters["IdProyecto"].DefaultValue =IdProyecto;
+            SqlDataSource13.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
             //SqlDataSource2.SelectParameters["IdProyecto"].DefaultValue = IdProyecto;
 
         }
