f5ae0595d7efe3e5e2d791d515f7703e0493a999
MariaCamilaUZ <mariacamila0871@gmail.com>
2017-10-25 11:32:40 -0500

    ahora si sin errores

--- GIDPI/scripts/audioAyuda.js
+++ GIDPI/scripts/audioAyuda.js
@@ -1,6 +1,5 @@
 ﻿var audio = document.getElementById("audio-player");
-var Nombre = document.getElementById("audio-nombre");
-var Perfil = document.getElementById("audio-Perfil");
+
 $('#ayudaAudioUno').click(function () {
     $('#audioAyudaUno').toggle();
     $(this).toggleClass('clientsClose');
@@ -37,16 +36,14 @@ $('#ayudaAudioSiete').click(function () {
 });
 
 $('#ayudaAudioOcho').click(function () {
-  
     $('#audioAyudaOcho').toggle();
-   $(this).toggleClass('clientsClose');
+    $(this).toggleClass('clientsClose');
 });
 
 $('#ayudaAudioNueve').click(function () {
-
+    audio.pause();
     $('#audioAyudaNueve').toggle();
     $(this).toggleClass('clientsClose');
-  
 });
 
 $('#ayudaAudioDiez').click(function () {
@@ -105,99 +102,6 @@ audio.addEventListener("timeupdate", function () {
     var duration = document.getElementById("duration");
     var s = parseInt(audio.currentTime % 60);
     var m = parseInt((audio.currentTime / 60) % 60);
-}, false);
-
-Waves.init();
-Waves.attach("#play-button", ["waves-button", "waves-float"]);
-Waves.attach("#pause-button", ["waves-button", "waves-float"]);
-
-
-
-//----------------------Nombre de proyecto ------------------------- 
-
-$("#pause-buttonNombre").click(function () {
-    $(this).children(".icon")
-      .toggleClass("icon-pause")
-      .toggleClass("icon-play");
-
-    if (Nombre.paused) {
-        Nombre.play();
-    } else {
-        Nombre.pause();
-    }
-});
-
-
-function CreateSeekBar() {
-    var seekbar = document.getElementById("audioSeekBarNombre");
-    seekbar.min = 0;
-    seekbar.max = Nombre.duration;
-    seekbar.value = 0;
-}
-
-function EndofAudio() {
-    document.getElementById("audioSeekBarNombre").value = 0;
-}
-
-function audioSeekBar() {
-    var seekbar = document.getElementById("audioSeekBarNombre");
-    Nombre.currentTime = seekbar.value;
-}
-
-function SeekBar() {
-    var seekbar = document.getElementById("audioSeekBarNombre");
-    seekbar.value = audio.currentTime;
-}
-
-Nombre.addEventListener("timeupdate", function () {
-    var duration = document.getElementById("duration");
-    var s = parseInt(Nombre.currentTime % 60);
-    var m = parseInt((Nombre.currentTime / 60) % 60);
-}, false);
-
-Waves.init();
-Waves.attach("#play-button", ["waves-button", "waves-float"]);
-Waves.attach("#pause-button", ["waves-button", "waves-float"]);
-
-//----------------Perfil del proyecto--------------------------------
-$("#pause-buttonPerfil").click(function () {
-    $(this).children(".icon")
-      .toggleClass("icon-pause")
-      .toggleClass("icon-play");
-
-    if (Perfil.paused) {
-        Perfil.play();
-    } else {
-        Perfil.pause();
-    }
-});
-
-
-function CreateSeekBar() {
-    var seekbar = document.getElementById("audioSeekBarPerfil");
-    seekbar.min = 0;
-    seekbar.max = Perfil.duration;
-    seekbar.value = 0;
-}
-
-function EndofAudio() {
-    document.getElementById("audioSeekBarPerfil").value = 0;
-}
-
-function audioSeekBar() {
-    var seekbar = document.getElementById("audioSeekBarPerfil");
-    Perfil.currentTime = seekbar.value;
-}
-
-function SeekBar() {
-    var seekbar = document.getElementById("audioSeekBarPerfil");
-    seekbar.value = Perfil.currentTime;
-}
-
-Perfil.addEventListener("timeupdate", function () {
-    var duration = document.getElementById("duration");
-    var s = parseInt(Perfil.currentTime % 60);
-    var m = parseInt((Perfil.currentTime / 60) % 60);
 }, false);
 
 Waves.init();
